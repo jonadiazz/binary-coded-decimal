6ad32e4abbd70ed0e435a5f640480213c0fae2b9
jonadiazz <jonadiazz@hotmail.com>
2015-01-29 17:55:30 -0600

    BCD
    any one decimal numeral can be represented by a four bit pattern

--- binary-coded-decimal.pl
+++ binary-coded-decimal.pl
@@ -1,3 +1,15 @@
 #!/usr/bin/perl
 
+use Modern::Perl;
 
+print "Please enter a binary number up to 30 digits: ";
+chomp (my $binaryNo = my $backupNo = <> );
+chomp $backupNo;
+
+my $exp = 1, my $decimal;
+while ( $binaryNo ) {
+   $decimal += chop($binaryNo) * $exp;
+   $exp *= 2;
+}
+
+print $backupNo.' is '.$decimal.' in decimal.'."\n\n";
